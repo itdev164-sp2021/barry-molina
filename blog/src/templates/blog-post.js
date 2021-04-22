93320c35c46b3dee447b2ca9cf83151103ef5a21
Barry Molina <bazzaboy@gmail.com>
2021-04-22 12:44:32 -0500

    Completed Activity 9

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Image } from 'rebass'
 import Layout from '../components/layout';
 import { H1 } from '../components/Heading'
 
 const BlogPost = ({ data }) => {
-    const { title, body } = data.contentfulBlogPost;
+    const { title, body, heroImage } = data.contentfulBlogPost;
 
     return (
         <Layout>
+            <Image src={heroImage.fluid.src} />
             <H1>{title}</H1>
             <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
         </Layout>
@@ -18,12 +20,17 @@ export default BlogPost;
 
 export const pageQuery = graphql`
 query blogPostQuery($slug: String!) {
-    contentfulBlogPost(slug: {eq: $slug}) {
+  contentfulBlogPost(slug: {eq: $slug}) {
     title
     slug
     body {
       childMarkdownRemark {
         html
+      }
+    }
+    heroImage {
+      fluid(maxWidth: 960) {
+        src
       }
     }
   }
